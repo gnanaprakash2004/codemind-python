26a69a966edd451cfd5d0a1ce729f026c6a337e9
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-05-18 09:04:16 +0530

    def fib(n):     if n==1:         print('0')     elif n==2:         print('0'+' '+'1')     else:         a,b=0,1         print(a,b,end=' ')         for i in range (2,n):             c=a+b             print(c,end=' ')             a=b             b=c n=int(input()) fib(n)

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+def fib(n):
+    if n==1:
+        print('0')
+    elif n==2:
+        print('0'+' '+'1')
+    else:
+        a,b=0,1
+        print(a,b,end=' ')
+        for i in range (2,n):
+            c=a+b
+            print(c,end=' ')
+            a=b
+            b=c
+n=int(input())
+fib(n)
