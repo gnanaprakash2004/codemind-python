4d5c93b892f726c05ff706777de174ac6f4053f8
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-06-08 13:17:32 +0530

    a,b=map(int,input().split()) ar1=list(map(int,input().split())) ar2=list(map(int,input().split())) s=[] for i in ar1:     if i not in ar2:         if i not in s:             s.append(i)         ##print(s) for i in ar2:     if i not in ar1:         if i not in s:             s.append(i)        ## print(s) print(len(s))

---
+++ Count_elements_II.py
@@ -0,0 +1,15 @@
+a,b=map(int,input().split())
+ar1=list(map(int,input().split()))
+ar2=list(map(int,input().split()))
+s=[]
+for i in ar1:
+    if i not in ar2:
+        if i not in s:
+            s.append(i)
+        ##print(s)
+for i in ar2:
+    if i not in ar1:
+        if i not in s:
+            s.append(i)
+       ## print(s)
+print(len(s))
