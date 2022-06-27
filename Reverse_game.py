88f4473f02e032f14a9b8a4b3bb7c431ba188dd9
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-06-27 22:45:20 +0530

    n=int(input()) a=list(map(int,input().split())) for i in range(n):     z=str(a[i])[::-1]     print(int(z),end=' ')

--- Reverse_game.py
+++ Reverse_game.py
@@ -1,6 +1,5 @@
 n=int(input())
-arr=list(map(int,input().split()))
+a=list(map(int,input().split()))
 for i in range(n):
-    m=str(arr[i])
-    z=int(m[::-1])
-    print(z,end=' ')
+    z=str(a[i])[::-1]
+    print(int(z),end=' ')
