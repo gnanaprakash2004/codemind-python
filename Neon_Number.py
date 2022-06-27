f7061823ab3b2b2fb4c8ff9c87b76483227e06e0
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-06-27 22:01:06 +0530

    n=int(input()) m=n*n s=0 while m>0:     r=m%10     s+=r     m=m//10 if(s==n):     print("Neon Number") else:     print("Not Neon Number")

--- Neon_Number.py
+++ Neon_Number.py
@@ -1,13 +1,11 @@
-import math
 n=int(input())
 m=n*n
-temp=m
-sum=0
-while(temp>0):
-    r=temp%10
-    sum=sum+r
-    temp=temp//10
-if(sum==n):
-    print('Neon Number')
+s=0
+while m>0:
+    r=m%10
+    s+=r
+    m=m//10
+if(s==n):
+    print("Neon Number")
 else:
-    print('Not Neon Number')
+    print("Not Neon Number")
