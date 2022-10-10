43f2f4d8fd0b1c48c518c3f513788e87116e0434
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-10-10 20:16:56 +0530

    t=int(input()) for i in range(t):
        n=int(input())
        arr=list(map(int,input().split()))
        c=0
        for i in range(n):
            if(arr[i]%2==1):
                c+=1
        if(c%2==1):
            c-=1
        print(c//2)

---
+++ Weird_Palindrome_Making.py
@@ -0,0 +1,12 @@
+t=int(input())
+for i in range(t):
+    
+    n=int(input())
+    arr=list(map(int,input().split()))
+    c=0
+    for i in range(n):
+        if(arr[i]%2==1):
+            c+=1
+    if(c%2==1):
+        c-=1
+    print(c//2)
