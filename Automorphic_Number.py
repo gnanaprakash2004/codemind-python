4026e414b3121dfdfab91ca3c22664fc7f4f613a
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-10-11 15:18:45 +0530

    import math n=int(input()) g=n r=n*n c=0 while n>0 :     v=n%10     c=c+1     n=n//10 x=r% math.pow(10,c) if(x==g) :     print('Automorphic Number') else :     print('Not an Automorphic Number')

---
+++ Automorphic_Number.py
@@ -0,0 +1,14 @@
+import math
+n=int(input())
+g=n
+r=n*n
+c=0
+while n>0 :
+    v=n%10
+    c=c+1
+    n=n//10
+x=r% math.pow(10,c)
+if(x==g) :
+    print('Automorphic Number')
+else :
+    print('Not an Automorphic Number')
