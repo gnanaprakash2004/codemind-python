4dc3cec3207af1fd8f2d798e0695b61cb63e3424
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-10-15 00:50:25 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() for i in s1:     if i not in s2:         print(False)         break else:     print(True)

--- Anagrams.py
+++ Anagrams.py
@@ -1,12 +1,10 @@
-s1=str(input())
-s2=str(input())
-c=0
-for i in s1.lower():
-    if i in s2.lower():
-        c+=1
-    else:
-        c+=0
-if(c==len(s1) or c==len(s2)):
-    print("True")
+s1=input()
+s2=input()
+s1=s1.lower()
+s2=s2.lower()
+for i in s1:
+    if i not in s2:
+        print(False)
+        break
 else:
-    print("False")
+    print(True)
