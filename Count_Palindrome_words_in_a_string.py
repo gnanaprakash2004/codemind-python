802129a7186d95751f105651678e7d3538f18b36
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-10-15 00:57:18 +0530

    a=list(map(str,input().split())) c=0 for s in a:
        s.lower()
        v=s[::-1]
        if v.lower()==s.lower() :
            c+=1
    print(c)

--- Count_Palindrome_words_in_a_string.py
+++ Count_Palindrome_words_in_a_string.py
@@ -1,23 +1,9 @@
-def pal(s):
-    s1=""
-    for i in s:
-        if i.isupper():
-            s1+=i.lower()
-        else:
-            s1+=i
-    if s1==rev(s1):
-        return 1
-    else:
-        return 0
-def rev(s):
-    s1=""
-    for i in range(len(s)-1,-1,-1):
-        s1+=s[i]
-    return s1
-a=input()
-arr=list(a.split())
-c=0
-for i in arr:
-    if pal(i)==1:
+a=list(map(str,input().split()))
+c=0 
+for s in a:
+    
+    s.lower()
+    v=s[::-1]
+    if v.lower()==s.lower() :
         c+=1
 print(c)
