eaa0651a0077665be0db8a0ed65a357453959fff
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-12-07 12:07:03 +0530

    n=int(input()) a=list(map(int,input().split())) s=0 p=n-1 for i in range(n):     if(a[i]==1):         s+=2**p     p-=1 print(s)

--- Binary_Array_to_decimal.py
+++ Binary_Array_to_decimal.py
@@ -1,8 +1,11 @@
 n=int(input())
 a=list(map(int,input().split()))
-j=0
 s=0
-for i in range(n-1,-1,-1):
-    s+=a[i]*(2**j)
-    j+=1
+p=n-1
+for i in range(n):
+    if(a[i]==1):
+        s+=2**p
+    p-=1
 print(s)
+        
+    
