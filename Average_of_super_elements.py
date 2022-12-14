63b18803e9bed958e9a51b152f7cdc48b9599a66
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-12-14 11:47:38 +0530

    n=int(input()) l=list(map(int,input().split())) b=[] for i in range(n):     if(l.count(l[i])==l[i]):         b.append(l[i]) b=set(b) if(len(b)==0):     print(-1) else:     print('%.2f' %(sum(b)/len(b)))

--- Average_of_super_elements.py
+++ Average_of_super_elements.py
@@ -3,11 +3,9 @@ l=list(map(int,input().split()))
 b=[]
 for i in range(n):
     if(l.count(l[i])==l[i]):
-        #print(l[i])
         b.append(l[i])
+b=set(b)
 if(len(b)==0):
     print(-1)
 else:
-        
-    b=set(b)
-    print('%.2f'%(sum(b)/len(b)))
+    print('%.2f' %(sum(b)/len(b)))
